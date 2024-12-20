431f02763db0c7f90bf6e376500df61d956c7015
stry26 <km440685@gmail.com>
2024-12-20 20:11:17 +0700

    fitur pertama : tiket

---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../component/navbar";
+
+const Home = () => {
+    return <>
+    <Navbar />
+    <h3>mmk</h3>
+    </>
+}
+
+export default Home
---
+++ src/pages/tiketpage.jsx
@@ -0,0 +1,11 @@
+import Tickets from "../component/tickets";
+import Navbar from "../component/navbar";
+
+const tiketPage = () => {
+    return <>
+    <Navbar />
+    <Tickets />
+    </>
+}
+
+export default tiketPage
