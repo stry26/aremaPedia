e1e497813bce3c3a60271e8e9d896bd5c084408e
stry26 <km440685@gmail.com>
2025-01-06 00:33:59 +0700

    up profile

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,13 @@ import Navbar from "../component/navbar";
 import Footer from "../component/footer";
 import Banner from "../component/banner";
 import News from "../component/news";
+import Profile from "../component/profile";
 
 const Home = () => {
     return <>
     <Navbar />
     <Banner />
+    <Profile />
     <News />
     <Footer />
     </>
