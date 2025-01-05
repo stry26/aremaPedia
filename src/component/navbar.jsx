218fe6d344e8ea8a9fac52db98e5cd75e60a6c87
stry26 <km440685@gmail.com>
2025-01-05 18:36:10 +0700

    up store page

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -23,8 +23,8 @@ export default function Navbar() {
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-            <li><a href='/'>Schedule</a></li>
-            <li><a href='/'>Store</a></li>
+            <li><a href='/schedule'>Schedule</a></li>
+            <li><a href='/store'>Store</a></li>
             <li><a href='/tiket'>Ticket</a></li>
           </ul>
         </div>
